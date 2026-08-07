5eee24189b6816f0458d4a1f1fd0f46cc19b215f
vuedj <aryamane.aniket@gmail.com>
2026-08-07 17:08:48 +0530

    Adding pinia store and vue router

---
+++ src/components/LeftSidebar.vue
@@ -0,0 +1,3 @@
+<template>
+	<aside class="flex-none bg-slate-300 max-md:order-0 md:w-1/6">L. Sidebar</aside>
+</template>
---
+++ src/layouts/Default.vue
@@ -0,0 +1,26 @@
+<script setup>
+import LeftSidebar from '@/components/LeftSidebar.vue'
+import RightSidebar from '@/components/RightSidebar.vue'
+</script>
+
+<template>
+	<div class="mx-auto flex min-h-screen max-w-[calc(100svw-16rem)] flex-col">
+		<header class="flex items-center justify-end gap-2 bg-red-400">
+			<router-link :to="{ name: 'home' }">Home</router-link>
+			<router-link :to="{ name: 'about' }">About</router-link>
+		</header>
+		<main class="flex flex-1 flex-col md:flex-row">
+			<LeftSidebar />
+			<div class="flex flex-1 flex-col max-md:order-2 md:flex-row">
+				<div class="flex-1 bg-slate-400">
+					<h1>Page Content</h1>
+					<router-view />
+				</div>
+			</div>
+			<RightSidebar />
+		</main>
+		<footer class="bg-green-400">Social links</footer>
+	</div>
+</template>
+
+<style scoped></style>
